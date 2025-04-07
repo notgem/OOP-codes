ff638fd1e52370ef9b67871449fc776a649a6716
notgem <shlok2918@gmail.com>
2025-04-08 00:54:58 +0530

    Create Student2.java

---
+++ edumanipal/mit/Student2.java
@@ -0,0 +1,22 @@
+package edu.manipal.mit;
+public class Student2 {
+
+	private String name;
+	private int roll;
+	
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public void setRollNumber(int roll){
+		this.roll = roll;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public int getRollNumber(){
+		return roll;
+	}
+}
